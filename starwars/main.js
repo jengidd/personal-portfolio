abd8330a54737b182744bbb5511bbc1e2362bf7f
Jena Giddings <jlg@jenas-mbp.ad.uvu.edu>
2020-02-27 12:44:01 -0700

    added buttons and css

--- starwars/main.js
+++ starwars/main.js
@@ -3,21 +3,41 @@ import { people } from '../data/people.js'
 import { starships } from '../data/starships.js'
 
 const greetingDiv = document.querySelector('.greeting')
+const maleButton = document.querySelector('#maleButton')
+const femaleButton = document.querySelector('#femaleButton')
+const otherButton = document.querySelector('#otherButton')
 
-const castList = document.createElement("ul")
+const maleCharacters = people.filter(person => person.gender === "male")
+console.log(maleCharacters)
+
+const femaleCharacters = people.filter(person => person.gender === "female")
+console.log(femaleCharacters)
+
+const otherCharacters = people.filter(person => {
+    if (person.gender === "hermaphrodite" || person.gender === "n/a") {
+        return person
+    }
+})
+
+console.log(otherCharacters)
 
 let counter = 1 
 
 people.forEach(person => {
-    let listItem = document.createElement("li")
-    listItem.textContent = person.name
-    castList.appendChild(listItem)
 
     let anchorWrap = document.createElement("a")
     anchorWrap.href = "#"
 
     let imageItem = document.createElement("img")
     imageItem.src = `https://starwars-visualguide.com/assets/img/characters/${counter}.jpg`
+
+    imageItem.addEventListener('error', (event) => {
+        //console.log(`$event.type): Loading image\n`)
+        //console.log(event)
+        imageItem.hidden = true
+        //imageItem.src = '../images/uvu-logo.png'
+    })
+
     // add some way to handle user clicks on the image
     imageItem.addEventListener("click", () => {
         console.log("It Worked")
@@ -27,4 +47,6 @@ people.forEach(person => {
     counter++
 })
 
-greetingDiv.appendChild(castList)
+maleButton.addEventListener("click", (event) => {
+    console.log("Clicked on")
+})
