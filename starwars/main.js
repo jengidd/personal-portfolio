b5fcb2d22863c0184765e2ecd281196675f7b37e
Jena Giddings <jena.giddings@gmail.com>
2020-03-05 12:46:52 -0700

    removed children, added starship page

--- starwars/main.js
+++ starwars/main.js
@@ -2,7 +2,7 @@ import { films } from '../data/films.js'
 import { people } from '../data/people.js'
 import { starships } from '../data/starships.js'
 
-const greetingDiv = document.querySelector('.greeting')
+const gallery = document.querySelector('.gallery')
 const maleButton = document.querySelector('#maleButton')
 const femaleButton = document.querySelector('#femaleButton')
 const otherButton = document.querySelector('#otherButton')
@@ -40,9 +40,16 @@ function getCharNumber(url) {
     return url.slice(start, end)
 }
 
+function removeChildren(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
+
 //getCharNumber("https://swapi.co/api/people/1/")
 
 function populateDOM(characters) {
+    removeChildren(gallery)
     characters.forEach(person => {
         // need to extract the number from the person.url property
     let charNum = getCharNumber(person.url)
@@ -64,6 +71,6 @@ function populateDOM(characters) {
         console.log(event)
     })
     anchorWrap.appendChild(imageItem)
-    greetingDiv.appendChild(anchorWrap)
+    gallery.appendChild(anchorWrap)
 })
 }
