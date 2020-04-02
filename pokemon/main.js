6c823c62afe7cd4f8d58a6389beaccdd6c113f9d
Jena Giddings <jena.giddings@gmail.com>
2020-04-02 12:28:05 -0600

    basic pokemon data retrieval working

--- pokemon/main.js
+++ pokemon/main.js
@@ -1,1 +1,12 @@
-console.log("pokemon")
+function getPokeData(url) {
+    let pokemonGrid = document.querySelector('.pokemonGrid')
+    fetch(url).then(function (response) {
+        let pokeData = response.json().then(function (data) {
+            console.log(data)
+            pokemonGrid.textContent = data.name
+        })
+        
+    })
+}
+
+getPokeData('https://pokeapi.co/api/v2/pokemon/1/')
