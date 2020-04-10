b1cf60266c3140242a3ebe23c422b9ff2602a9a9
Jena Giddings <jena.giddings@gmail.com>
2020-04-09 20:41:31 -0600

    added images, slight card styling

--- pokemon/main.js
+++ pokemon/main.js
@@ -1,20 +1,3 @@
-/*const allPokemon = []
-
-function getPokeData(url) {
-  fetch(url).then(function (response) {
-    response.json().then(function (pokeData) {
-      console.log(pokeData.results)
-      const pokeMap = pokeData.results.map(pokemon => {
-        return fetch(pokemon.url).then(resData => {
-          resData.json().then(pokeJson => {
-            allPokemon.push(pokeJson)
-          })
-
-        })
-      })
-    })
-  })
-} */
 
 //note line
 async function getAPIData(url) {
@@ -31,16 +14,14 @@ async function getAPIData(url) {
 getAPIData('https://pokeapi.co/api/v2/pokemon/?&limit=25').then((data) => {
   for (const pokemon of data.results) {
     getAPIData(pokemon.url).then((pokeData) => {
-      populatePokeCards(pokeData)
+      populatePokeCard(pokeData)
     })
   }
 })
 
 let pokemonGrid = document.querySelector('.pokemonGrid')
 
-//getPokeData('https://pokeapi.co/api/v2/pokemon?&limit=25')
-
-function populatePokeCards(singlePokemon) {
+function populatePokeCard(singlePokemon) {
   let pokeScene = document.createElement('div')
   pokeScene.className = 'scene'
   let pokeCard = document.createElement('div')
@@ -48,12 +29,8 @@ function populatePokeCards(singlePokemon) {
   pokeCard.addEventListener('click', () =>
     pokeCard.classList.toggle('is-flipped'),
   )
-  let pokeFront = document.createElement('div')
-  pokeFront.className = 'card__face card__face--front'
-  pokeFront.textContent = singlePokemon.name
-  let pokeBack = document.createElement('div')
-  pokeBack.className = 'card__face card__face--back'
-  pokeBack.textContent = 'back'
+  let pokeFront = populateCardFront(singlePokemon)
+  let pokeBack = populateCardBack(singlePokemon)
 
   pokeCard.appendChild(pokeFront)
   pokeCard.appendChild(pokeBack)
@@ -61,16 +38,26 @@ function populatePokeCards(singlePokemon) {
   pokemonGrid.appendChild(pokeScene)
 }
 
-/* var card = document.querySelector('.card');
-card.addEventListener('click', function() {
-  card.classList.toggle('is-flipped');
-}); */
-
-{
-  /* <div class="scene>"
-  <div class="card">
-    <div class="card__face card__face--front">front</div>
-    <div class="card__face card__face--back">back</div>
-  </div>
-  </div> */
+function populateCardFront(pokemon) {
+  let cardFront = document.createElement('div')
+  cardFront.className = 'card__face card__face--front'
+  cardFront.textContent = pokemon.name
+  let frontImage = document.createElement('img')
+  frontImage.src = `../images/${pokemon.id}.png`
+  cardFront.appendChild(frontImage)
+  return cardFront
 }
+
+function populateCardBack(pokemon) {
+  let cardBack = document.createElement('div')
+  cardBack.className = 'card__face card__face--back'
+  let abilityList = document.createElement('ul')
+  pokemon.abilities.forEach(ability => {
+    let abilityName = document.createElement('li')
+    abilityName.textContent = ability.ability.name
+    abilityList.appendChild(abilityName)
+  })
+  cardBack.appendChild(abilityList)
+  return cardBack
+}
+    
